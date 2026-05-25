235eea3ce15641313700c279ff99d8d86be362b8
GianMarcoDelaCruzBernardo <gian.delacruz@tecsup.edu.pe>
2026-05-25 10:06:16 -0500

    fix: CompressedStaticFilesStorage + collectstatic --clear para Render

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -e
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 python manage.py migrate
 
