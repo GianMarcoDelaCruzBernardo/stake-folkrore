fc696fcb4595f60c1285fbe4aadc9b5f60b525a2
GianMarcoDelaCruzBernardo <gian.delacruz@tecsup.edu.pe>
2026-05-28 01:19:47 -0500

    fix: separador de miles en saldo (intcomma) en navbar y tienda

--- stakefolclor/settings.py
+++ stakefolclor/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "cloudinary_storage",
     "cloudinary",
     "crispy_forms",
